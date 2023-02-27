767f5a42655b71ab57898b11bde4793a2874078a
hughkick <hughki@yahoo.com.tw>
2023-02-28 00:27:29 +0800

    combo box editable

---
+++ combo_eidt_box/combo_eidt_box/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace combo_eidt_box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void combo_heandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                box1.Items.Add(box1.Text);
+            }
+        }
+    }
+}
